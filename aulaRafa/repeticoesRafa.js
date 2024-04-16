6494c6e69f75efa8563ec2b20b842156960ca46b
KAUÃ DA ROCHA BRAGA DA SILVA <kaua_rb_silva@estudante.sesisenai.org.br>
2024-04-15 21:08:03 -0300

    Exercicio 3

---
+++ aulaRafa/repeticoesRafa.js
@@ -0,0 +1,49 @@
+let resultadoBruto = document.getElementById('divResultado')
+let cont = 1
+while(cont <= 10){
+    resultadoBruto *= cont
+    cont++
+}
+resultadoBruto = "Resultado da Soma" + resultadoBruto
+// let res = document.getElementById("divResultado")
+// console.log(res.innerHTML);
+// // res.innerHTML = 'mudei'
+// // console.log(document.getElementById("divResultado").innerHTML)
+// let soma = 0
+// let cont = 1
+// while(cont <= 100){
+//     // res.innerHTML += cont + "<br>"
+//     soma += cont
+//     cont++
+//     res.innerHTML = "Resultado da soma: " + soma
+// }
+
+// Faça um programa que calcule o quadrado de cada número de 1 a 10.
+// let resultadoBruto
+// let cont = 1
+// while(cont <= 10){
+//     resultadoBruto *= cont
+//     cont++
+//     resultadoBruto = "Resultado da Soma" + resultadoBruto
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
